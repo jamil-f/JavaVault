414459d05513c8ee90ccf600283556ad1118b1d4
Jamil Ford <jamilford@Jamils-MacBook-Pro.local>
2024-05-15 03:23:12 -0400

    First Commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const str1 =  40 - 30;
+//Subtraction
+const str2 = 10 * 4;
+//Multiplication
+const str3 = 32 + 7;
+//Addition
+const note = "You have received this message because you have been chosen to open an important vault. Here is the secret combination: "
++ str1 + " - " + str2 + " - " + str3;
+//Created 3 variables and concatenated those variables into note. Perform alert on note
+alert(note)
